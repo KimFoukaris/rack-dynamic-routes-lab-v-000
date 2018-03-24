f983b77a9987f1c340b64374894b3bd008c67545
Kim Foukaris <kimfoukaris@outlook.com>
2018-03-24 14:54:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-    check_item = req.params["item"]
-    if req.path == "/items/#{check_item}"
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last 
+      item = @@items.find{|i| i.title == item_name}
       if @@items.include?(check_item)
         resp.write "3.42"
         resp.status = 200
@@ -12,7 +14,7 @@ class Application
         resp.status = 400
       end
     else
-      resp.write "Route not found"
+      #resp.write "Route not found"
       resp.status = 404
     end
 
